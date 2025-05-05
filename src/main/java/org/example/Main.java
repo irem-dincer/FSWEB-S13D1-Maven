99004b911bb38da6a432e3c636c6b7cecbabb092
irem-dincer <irem.dincer02@gmail.com>
2025-05-05 15:53:39 +0300

    should Wake Up is done and user Id has changed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,41 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+
+        if(clock<0 ||clock>23) {
+            return false;
+        }
+      if(isBarking && (clock<8 || clock>20)){   //
+           return  true;
+       }
+       return false;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if(firstAge>13 && firstAge<19){
+            return true;
+        }
+        if(secondAge>13 && secondAge<19){
+            return true;
+        }
+        if(thirdAge>13 && thirdAge<19){
+            return true;
+        }
+        return false;
+
     }
 
-    public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
-    }
+    /*public static boolean isCatPlaying(boolean isSummer, int temp) {
 
-    public static double area(double width, double height) {
-       
+
+   }
+
+  public static double area(double width, double height) {
+
     }
 
     public static double area(double radius) {
-       
-    }
+
+    }*/
+
 }
